9db57dd292e19f9115a2fa65fd74895280ab9eb0
zhoushuai <shuai.zhou@avatr.com>
2022-11-01 19:30:55 +0800

    代码改造。实现本地零配置

--- scripts/build.bat
+++ scripts/build.bat
@@ -16,24 +16,24 @@ rem
 @echo off
 
 rem apollo config db info
-set apollo_config_db_url="jdbc:mysql://localhost:3306/ApolloConfigDB?characterEncoding=utf8"
+set apollo_config_db_url="jdbc:mysql://139.196.208.53:3306/ApolloConfigDB?characterEncoding=utf8"
 set apollo_config_db_username="root"
-set apollo_config_db_password=""
+set apollo_config_db_password="Zs11195310"
 
 rem apollo portal db info
-set apollo_portal_db_url="jdbc:mysql://localhost:3306/ApolloPortalDB?characterEncoding=utf8"
+set apollo_portal_db_url="jdbc:mysql://139.196.208.53:3306/ApolloPortalDB?characterEncoding=utf8"
 set apollo_portal_db_username="root"
-set apollo_portal_db_password=""
+set apollo_portal_db_password="Zs11195310"
 
 rem meta server url, different environments should have different meta server addresses
-set dev_meta="http://localhost:8080"
+set dev_meta="http://139.196.208.53:8080"
 set fat_meta="http://someIp:8080"
 set uat_meta="http://anotherIp:8080"
 set pro_meta="http://yetAnotherIp:8080"
 
 set META_SERVERS_OPTS=-Ddev_meta=%dev_meta% -Dfat_meta=%fat_meta% -Duat_meta=%uat_meta% -Dpro_meta=%pro_meta%
 
-rem =============== Please do not modify the following content =============== 
+rem =============== Please do not modify the following content ===============
 rem go to script directory
 cd "%~dp0"
 
